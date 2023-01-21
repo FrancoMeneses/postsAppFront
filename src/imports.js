490d7f7375276c863dbcbf3b408fbe95de6deae0
Franco Meneses <meneses773@gmail.com>
2023-01-20 18:53:00 -0600

    imports created

---
+++ src/imports.js
@@ -0,0 +1,6 @@
+import { Navbar } from "./components/navbar"
+import { NewForm } from "./pages/newForm"
+import { Home } from "./pages/home"
+import { NotFound } from "./pages/notFound"
+
+export { Navbar, NewForm, Home, NotFound }
